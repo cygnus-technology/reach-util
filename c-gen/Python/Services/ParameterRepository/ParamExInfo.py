a036b968f5636efd36456f771a174fd6fa4097fd
acarlson-i3 <155561614+acarlson-i3@users.noreply.github.com>
2024-05-01 10:22:43 -0500

    Remove shorthand code from ParamExInfo.py

--- c-gen/Python/Services/ParameterRepository/ParamExInfo.py
+++ c-gen/Python/Services/ParameterRepository/ParamExInfo.py
@@ -1,14 +1,8 @@
 import json
 from ... import util
 
-make_c_compatible = util.make_c_compatible
-gen_enum = util.gen_enum
-gen_c_array = util.gen_c_array
-gen_c_struct = util.gen_c_struct
-
-
 def to_param_ei_enum(pei: json):
-    return make_c_compatible(f"PARAM_EI_{pei['name']}", upper=True)
+    return util.make_c_compatible(f"PARAM_EI_{pei['name']}", upper=True)
 
 
 def to_label_enums(pei: json):
@@ -23,7 +17,7 @@ def to_label_enums(pei: json):
             for enum in pei['enumValues']:
                 enums.append(enum['label'])
                 values.append(enum['id'])
-            return gen_enum(enums, values, pei['name'], transform_enum_names=True)
+            return util.gen_enum(enums, values, pei['name'], transform_enum_names=True)
         case "bitfield":
             # Generate both the index and the value enumerations
             enums = []
@@ -33,11 +27,11 @@ def to_label_enums(pei: json):
             for enum in pei['bitIndices']:
                 enums.append(enum['label'])
                 index_values.append(enum['id'])
-                temp = make_c_compatible(f"{name} indices {enum['label']}", upper=True)
+                temp = util.make_c_compatible(f"{name} indices {enum['label']}", upper=True)
                 bit_values.append(f"0b1 << {temp}")
-            output = gen_enum(enums, index_values, f"{name} indices", transform_enum_names=True)
+            output = util.gen_enum(enums, index_values, f"{name} indices", transform_enum_names=True)
             output.append("")
-            output += gen_enum(enums, bit_values, name, transform_enum_names=True)
+            output += util.gen_enum(enums, bit_values, name, transform_enum_names=True)
             return output
         case _:
             raise ValueError(f"Unexpected dataType {pei['dataType']}")
@@ -46,24 +40,24 @@ def to_label_enums(pei: json):
 def to_local_array(pei: json, depth=0):
     match pei['dataType']:
         case "boolean":
-            elements = [gen_c_struct([{"field": "id", "value": 0},
+            elements = [util.gen_c_struct([{"field": "id", "value": 0},
                                      {"field": "name", "value": f"\"{pei['falseLabel']}\""}], depth=depth),
-                        gen_c_struct([{"field": "id", "value": 1},
+                        util.gen_c_struct([{"field": "id", "value": 1},
                                      {"field": "name", "value": f"\"{pei['trueLabel']}\""}], depth=depth)]
         case "enumeration":
             elements = []
             for enum in pei['enumValues']:
-                elements.append(gen_c_struct([{"field": "id", "value": enum['id']},
+                elements.append(util.gen_c_struct([{"field": "id", "value": enum['id']},
                                              {"field": "name", "value": f"\"{enum['label']}\""}], depth=depth))
         case "bitfield":
             elements = []
             for bit in pei['bitIndices']:
-                elements.append(gen_c_struct([{"field": "id", "value": bit['id']},
+                elements.append(util.gen_c_struct([{"field": "id", "value": bit['id']},
                                              {"field": "name", "value": f"\"{bit['label']}\""}], depth=depth))
         case _:
             raise ValueError(f"Unexpected dataType {pei['dataType']}")
-    output = gen_c_array(elements, 0)
-    output[0] = f"static const cr_ParamExKey __cr_gen_pei_{make_c_compatible(pei['name']).lower()}_labels = {output[0]}"
+    output = util.gen_c_array(elements, 0)
+    output[0] = f"static const cr_ParamExKey __cr_gen_pei_{util.make_c_compatible(pei['name']).lower()}_labels = {output[0]}"
     output[-1] += ";"
     return output
 
@@ -88,5 +82,5 @@ def to_struct(pei: json, depth=0):
         case _:
             raise ValueError(f"Unexpected dataType {pei['dataType']}")
     fields.append({"field": "num_labels", "value": num_labels})
-    fields.append({"field": "labels", "value": f"__cr_gen_pei_{make_c_compatible(pei['name']).lower()}_labels"})
-    return gen_c_struct(fields, depth=depth)
+    fields.append({"field": "labels", "value": f"__cr_gen_pei_{util.make_c_compatible(pei['name']).lower()}_labels"})
+    return util.gen_c_struct(fields, depth=depth)
