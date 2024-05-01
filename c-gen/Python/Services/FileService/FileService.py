c3917f25b91c15a5adc1183cde46bdef1417a6df
acarlson-i3 <155561614+acarlson-i3@users.noreply.github.com>
2024-05-01 12:50:50 -0500

    Start populating the FileService value generation

--- c-gen/Python/Services/FileService/FileService.py
+++ c-gen/Python/Services/FileService/FileService.py
@@ -2,8 +2,28 @@ import json
 from ... import util
 
 class File:
+    access_levels = {
+        "None": "cr_AccessLevel_NO_ACCESS",
+        "Read": "cr_AccessLevel_READ",
+        "Write": "cr_AccessLevel_WRITE",
+        "Read/Write": "cr_AccessLevel_READ_WRITE"
+    }
+
+    storage_locations = {
+        "RAM": "cr_StorageLocation_RAM",
+        "Extended RAM": "cr_StorageLocation_RAM_EXTENDED",
+        "NVM": "cr_StorageLocation_NONVOLATILE",
+        "Extended NVM": "cr_StorageLocation_NONVOLATILE_EXTENDED"
+    }
+
     def to_enum(param: json):
         return util.make_c_compatible(f"FILE_{param['name']}", upper=True)
+
+    def to_protobuf(param: json, depth=0):
+        fields = [{"field": "file_id", "value": File.to_enum(param)}, {"field": "file_name", "value": f"\"{param['name']}\""}]
+        return util.gen_c_struct(fields, depth=depth)
+
+
 
 def gen_definitions(service: json):
     lines = ["#define INCLUDE_FILE_SERVICE"]
@@ -20,4 +40,16 @@ def gen_enums(service: json):
             enums.append(File.to_enum(file))
             values.append(file['id'])
         output.append(util.gen_enum(enums, values, "file"))
+    return output
+
+def gen_variables(service: json):
+    output = []
+    if len(service['files']) > 0:
+        structs = []
+        for file in service['files']:
+            structs.append(File.to_protobuf(file))
+        lines = util.gen_c_array(structs)
+        lines[0] = f"static const cr_FileInfo file_descriptions[NUM_FILES] = {lines[0]}"
+        lines[-1] += ';'
+        output.append(lines)
     return output
