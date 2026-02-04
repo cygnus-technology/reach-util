56c55b9140f32e8fe0792e0dd1b5abe7cdd49809
chuckpeplinski <chuck.peplinski@i3pd.com>
2026-02-04 14:04:18 -0600

    Updated the cli.c template file to support the command line history feature.

--- c-gen/Templates/cli.c
+++ c-gen/Templates/cli.c
@@ -13,7 +13,7 @@
 
 /* Template code start [.c Defines] */
 #ifndef CLI_MAX_LINE_LENGTH
-#define CLI_MAX_LINE_LENGTH 64
+#error CLI_MAX_LINE_LENGTH should be defined in reach_server.h
 #endif // CLI_MAX_LINE_LENGTH
 /* Template code end [.c Defines] */
 
@@ -29,6 +29,9 @@
 /* Template code start [.c Local/Extern Variables] */
 static char sInput[CLI_MAX_LINE_LENGTH];
 static uint8_t sInputLength = 0;
+#if (NUM_CMD_MEMORIES > 0) // 0 if undefined
+  static size_t sInputIndex = 0;
+#endif
 /* Template code end [.c Local/Extern Variables] */
 
 /* Template code start [.h Global Functions] */
@@ -38,6 +41,78 @@ void cli_init(void)
 	/* User code start [CLI: Init] */
 	/* User code end [CLI: Init] */
 }
+
+#if (NUM_CMD_MEMORIES > 0)
+/**
+ * Clears line
+ */
+void clear_line()
+{
+	i3_log(LOG_MASK_BARE|LOG_MASK_ALWAYS, EMPTY_LINE);
+}
+
+/**
+ * Goes to previous command in history.
+ */
+void process_up()
+{
+	// Go back in command history
+	cmd_hist_back(sInput, &sInputIndex);
+	sInputLength = sInputIndex;
+
+	// Display new command
+	clear_line();
+	i3_log(LOG_MASK_BARE|LOG_MASK_ALWAYS, "%c", CARRIAGE_RETURN);
+	i3_log(LOG_MASK_BARE|LOG_MASK_ALWAYS, "%c", PROMPT);
+	i3_log(LOG_MASK_BARE|LOG_MASK_ALWAYS, sInput, sInputIndex);
+}
+
+/**
+ * Goes to more recent command in history.
+ */
+void process_down()
+{
+	// Go back in command history
+	cmd_hist_fwd(sInput, &sInputIndex);
+	sInputLength = sInputIndex;
+
+	// Display new command
+	clear_line();
+	i3_log(LOG_MASK_BARE|LOG_MASK_ALWAYS, "%c", CARRIAGE_RETURN);
+	i3_log(LOG_MASK_BARE|LOG_MASK_ALWAYS, "%c", PROMPT);
+	i3_log(LOG_MASK_BARE|LOG_MASK_ALWAYS, sInput, sInputIndex);
+}
+
+/**
+ * Moves command line cursor to previous character in current command.
+ */
+void process_left()
+{
+	if (sInputIndex > 0)
+	{
+		// Move Cursor to the left
+		sInputIndex--;
+		i3_log(LOG_MASK_BARE|LOG_MASK_ALWAYS, CURSOR_LEFT, 4);
+	}
+}
+
+/**
+ * Moves command line cursor to previous character in current command.
+ */
+void process_right()
+{
+	if (sInputIndex < CLI_MAX_LINE_LENGTH)
+	{
+		// Move Cursor to the right
+		sInputIndex++;
+		i3_log(LOG_MASK_BARE|LOG_MASK_ALWAYS, CURSOR_RIGHT, 4);
+	}
+}
+
+// State machine for command history
+char_state_e char_state = IDLE;
+
+#endif // NUM_CMD_MEMORIES
 
 /**
  * Gets and processes data from the command line
@@ -52,6 +127,86 @@ bool cli_poll(void)
 		sInputLength = 0;
 		cli_write_prompt();
 	}
+#if (defined(NUM_CMD_MEMORIES) && (NUM_CMD_MEMORIES > 0))
+	char inChar;
+	if (cli_read_char(&inChar))
+	{
+		switch (char_state) {
+			// State machine handles special characters
+			case IDLE:
+				if (inChar == ESCAPE)
+				{
+					char_state = SPEC_CHAR;
+					break;
+				}
+				switch (inChar)
+				{
+					case '\r':
+						cli_write("\r\n");
+						if (sInputLength == 0)
+						{
+							cli_write_prompt();
+							break; // No data, no need to call anything
+						}
+						sInput[sInputLength] = 0; // Null-terminate the string
+						cmd_hist_cmd_exec(sInput);
+						crcb_cli_enter((const char*) sInput);
+						sInputLength = 0;
+						sInputIndex = 0;
+						memset(sInput, 0, sizeof(sInput));
+						cli_write_prompt();
+						break;
+					case '\n':
+						break; // Ignore, only expect '\r' for command execution
+					case '\b':
+						// Received a backspace
+						if (sInputLength > 0)
+						{
+							sInput[--sInputLength] = 0;
+							sInputIndex--;
+							cli_write("\b \b");
+						}
+						break;
+					default:
+						// Still waiting for an input
+						sInput[sInputIndex] = inChar;
+						cli_write_char(inChar);
+						if (sInputIndex < sizeof(sInput))
+							sInputIndex++;
+						if (sInputIndex > sInputLength)
+							sInputLength = sInputIndex;
+						break;
+				}
+				break;
+
+			case SPEC_CHAR:
+				if (inChar == '[')
+					char_state = ARROW_KEY;
+				else
+					char_state = IDLE;
+				break;
+
+			case ARROW_KEY:
+				// Process arrow key
+				if (inChar == 'A')
+					process_up();
+				else if (inChar == 'B')
+					process_down();
+				else if (inChar == 'C')
+					process_right();
+				else if (inChar == 'D')
+					process_left();
+				// fall through
+			default:
+			case MODIFY:
+				char_state = IDLE;
+				break;
+		}
+		// i3_log(LOG_MASK_ALWAYS, "     %d, %d", sInputLength, sInputIndex);
+		return true;
+	}
+
+#else // command line history is NOT used
 	if (cli_read_char(&sInput[sInputLength]))
 	{
 		switch (sInput[sInputLength])
@@ -86,8 +241,9 @@ bool cli_poll(void)
 					sInputLength++;
 				break;
 		}
-	return true;
+		return true;
 	}
+#endif  // NUM_CMD_MEMORIES
 	return false;
 }
 
@@ -111,7 +267,7 @@ static void cli_write(char *text)
 	/* User code start [CLI: Write]
 	 * This is where other output sources should be handled (for example, writing to a UART port)
 	 * This is called for outputs which are not necessary via BLE, such as clearing lines or handling backspaces */
-    (void)text;
+	(void)text;
 	/* User code end [CLI: Write] */
 }
 
@@ -119,7 +275,7 @@ static void cli_write_char(char c)
 {
 	/* User code start [CLI: Write Char]
 	 * This is used to write single characters, which may be handled differently from longer strings. */
-    (void)c;
+	(void)c;
 	/* User code end [CLI: Write Char] */
 }
 
@@ -128,9 +284,9 @@ static bool cli_read_char(char *received)
 	/* User code start [CLI: Read]
 	 * This is where other input sources (such as a UART) should be handled.
 	 * This should be non-blocking, and return true if a character was received, or false if not. */
-    (void)received;
+	(void)received;
 	/* User code end [CLI: Read] */
-    return false;   // Default implementation, no input available
+	return false;   // Default implementation, no input available
 }
 
 /* Template code end [.c Local Functions] */
